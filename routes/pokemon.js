166d145889d53d1987b5db5fcd49adda265ba0c0
mickeychcg <mcowanseattle@gmail.com>
2019-02-24 22:23:02 -0800

    it's working committing!

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,7 +5,7 @@ const db = require('../models');
 
 
 /* GET - / - gets all records from the DB and render to view */
-router.get('', function (req, res) {
+router.get('/', function (req, res) {
   db.pokemon.findAll().then(function (pokemons) {
     res.render('pokemon/pokedex', { pokemons })
   })
@@ -13,7 +13,7 @@ router.get('', function (req, res) {
 
 /* POST - /pokemon - receive the name of a 
 pokemon and add it to the database */
-router.post('', function (req, res) {
+router.post('/', function (req, res) {
   db.pokemon.create({
     name: req.body.name
   }).then(function () {
@@ -26,8 +26,8 @@ and displays card attributes */
 router.get("/:name", function (req, res) {
   let uri = ("http://pokeapi.co/api/v2/pokemon/" + req.params.name);
   request(uri, function (error, response, body) {
-    let stats = JSON.parse(body)
-    res.redirect('pokemon/show', { stats })
+    let api = JSON.parse(body)
+    res.render('pokemon/show', { api })
   });
 });
 
